a3c71969dadec2f6ac4767fe4bffb0f4fdafdbdf
suyouliang <suyouliang@qq.com>
2024-07-26 19:24:29 +0800

    日志监听处理器移动到框架层默认加载

--- demo-web/src/main/java/com/framework/demo/demos/web/listener/ApiLogListener.java
+++ demo-web/src/main/java/com/framework/demo/demos/web/listener/ApiLogListener.java
@@ -1,23 +1,26 @@
-package com.framework.demo.demos.web.listener;
-
-import com.ty.mid.framework.common.util.JsonUtils;
-import com.ty.mid.framework.web.core.model.event.ApiAccessLogEvent;
-import com.ty.mid.framework.web.core.model.event.ApiErrorLogEvent;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.event.EventListener;
-import org.springframework.stereotype.Component;
-
-@Component
-@Slf4j
-public class ApiLogListener {
-
-    @EventListener
-    public void onApiAccessLogEvent(ApiAccessLogEvent event) {
-        log.info("api access log eventData: \r\n{}", JsonUtils.toJson(event.getSource()));
-    }
-
-    @EventListener
-    public void onApiErrorLogEvent(ApiErrorLogEvent event) {
-        log.error("api error log eventData: {}", JsonUtils.toJson(event.getSource()));
-    }
-}
+/**
+ * 默认日志处理移动到框架层,如需自定义,参考一下代码,可覆盖默认日志监听处理器
+ */
+//package com.framework.demo.demos.web.listener;
+//
+//import com.ty.mid.framework.common.util.JsonUtils;
+//import com.ty.mid.framework.web.core.model.event.ApiAccessLogEvent;
+//import com.ty.mid.framework.web.core.model.event.ApiErrorLogEvent;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.context.event.EventListener;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//@Slf4j
+//public class ApiLogListener {
+//
+//    @EventListener
+//    public void onApiAccessLogEvent(ApiAccessLogEvent event) {
+//        log.info("api access log eventData: \r\n{}", JsonUtils.toJson(event.getSource()));
+//    }
+//
+//    @EventListener
+//    public void onApiErrorLogEvent(ApiErrorLogEvent event) {
+//        log.error("api error log eventData: {}", JsonUtils.toJson(event.getSource()));
+//    }
+//}
